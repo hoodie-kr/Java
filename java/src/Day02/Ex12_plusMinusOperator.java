8f73ae0e2111c9d6a8fb1fd1f788ae27bbfbf92a
tj-bu-706-25 <tj-bu-706-25@DESKTOP-2JTUAM2>
2025-03-12 18:07:18 +0900

    3일차

--- java/src/Day02/Ex12_plusMinusOperator.java
+++ java/src/Day02/Ex12_plusMinusOperator.java
@@ -6,9 +6,8 @@ public class Ex12_plusMinusOperator {
 		int a = 10;
 		int b = 20;
 		int c;
-		int x;
-		int y;
-		int z;
+		
+		
 		
 		// 증감연산자
 		// ++a, a++ : a를 1증가
@@ -18,7 +17,10 @@ public class Ex12_plusMinusOperator {
 		c = ++a + b;
 		System.out.println("c = " + c);
 		System.out.println("a = " + a);
-
+		
+		int x = 10;
+		int y = 20;
+		int z ;
 		// 후위연산자 : 연산자 우선순위가 가장 낮다.(a++, a--)
 		
 		z = x++ + y;
---
+++ java/src/Day03/Ex10_WhileSum.java
@@ -0,0 +1,29 @@
+package Day03;
+
+public class Ex10_WhileSum {
+
+	public static void main(String[] args) {
+		// 1부터 1000까지 합계를 구하시오.
+		// 1+2+3+.. +999+1000 = ????
+		
+		int a = 1;
+		int sum = 0;
+		
+		while(a<=1000) {
+			// 방법 1
+		    // sum += a+1;
+		    // a += 1;}
+			
+			// 방법 2
+			// sum = sum + a++;
+			
+			// 방법
+			// sum += a++;
+			
+		}
+		
+			System.out.println("1부터 1000까지 합계를 구하시오."+ sum);
+			
+		}
+	}
+
